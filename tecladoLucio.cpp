870c1618d8f2dfabe1420b9bc75e217799478987
lgvalentin <lgvalentin@professores.utfpr.edu.br>
2024-12-20 05:34:52 +0000

    Implementação básica do teclado

--- tecladoLucio.cpp
+++ tecladoLucio.cpp
@@ -8,12 +8,12 @@ void TecladoLucio::adicioneTecla(Tecla &tecla)
 void TecladoLucio::definaUcp(Ucp *ucp) { this->ucp = ucp; }
 Ucp* TecladoLucio::obtenhaUcp() { return this->ucp; }
 
-void TecladoLucio::recebaDigito(Digito){
-    
+void TecladoLucio::recebaDigito(Digito digito){
+  if(this->ucp) this->ucp->recebaDigito(digito);    
 }
-void TecladoLucio::recebaOperacao(Operacao){
-    
+void TecladoLucio::recebaOperacao(Operacao operacao){
+  if(this->ucp) this->ucp->recebaOperacao(operacao);        
 }
-void TecladoLucio::recebaControle(Controle){
-    
+void TecladoLucio::recebaControle(Controle controle){
+  if(this->ucp) this->ucp->recebaControle(controle);    
 }
